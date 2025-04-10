f2978517acaa23faabd40585bb9d2397931fc8da
Dac Trung Nguyen <dactrung.nguyen@gmail.com>
2025-04-10 21:31:59 +0200

    Send to caller by writing to stream

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::TcpListener;
-use std::io::Read;
+use std::io::{Read, Write};
 
 fn main() {
     // Bind the server to the address
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    println!("Server listening on http://localhost:3000");
+    let addr = "192.168.1.21:3000"; // "{interface}:{port}"
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("Server listening on {addr}");
     // Accept incoming connections
     let mut streams = listener.incoming();
     // Loop through the incoming connections
@@ -17,6 +18,6 @@ fn main() {
         let s = String::from_utf8_lossy(&buffer[..n]);
         println!("The bytes:\n{:?}", &buffer[..n]);
         println!("The string:\n{}", s);
+        stream.write_all(b"HTTP/1.1 200 OK\r\n\r\nHello, world!\r\n").unwrap();
     }
-    
 }
